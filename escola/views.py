8843d11748e191876d2058fd71a4827a1fff33b2
Matheus Roberto Aparecido de Souza <matheus.souza@casj.com.br>
2025-08-21 16:19:56 -0300

    Finalizado serializers e model de MATRICULA

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,27 @@
-from django.http import JsonResponse
+from escola.models import Estudante, Curso, Matricula
+from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasEstudanteSerializer, ListaMatriculasCursoSerializer
+from rest_framework import viewsets, generics
 
-def estudantes(request):
-    if request.method =='GET':
-        estudante = {
-            'id': '1',
-            'nome': 'Matheus'
-        }
-        return JsonResponse(estudante)
+class EstudanteViewSet(viewsets.ModelViewSet):
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer 
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+class ListaMatriculaEstudante(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasEstudanteSerializer
+
+class ListaMatriculaCurso(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasCursoSerializer
